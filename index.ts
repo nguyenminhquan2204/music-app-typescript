7c339d52792f42b3df4faee135098a7e98a6813b
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-07 21:27:00 +0700

    Kết nối database

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Chi    ")
+import express, { Request, Response, Express } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/topics", (req: Request, res: Response) => {
+
+    res.render("client/pages/topics/index");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
