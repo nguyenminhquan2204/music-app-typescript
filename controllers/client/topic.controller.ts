893f4e4b038004631ebd41cedaa43bcc272c676c
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-07 22:01:55 +0700

    Cấu trúc lại folder views

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -7,8 +7,9 @@ export const topics = async (req: Request, res: Response) => {
     const topics = await Topic.find({
         deleted: false
     });
-
-    console.log(topics);
     
-    res.render("client/pages/topics/index");
+    res.render("client/pages/topics/index", {
+        pageTitle: "Chủ đề bài hát",
+        
+    });
 };
