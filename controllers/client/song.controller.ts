108b26f2641effb45153664449aa950bbab536bb
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-08 21:25:47 +0700

    Làm trang chi tiết bài hát

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -38,3 +38,31 @@ export const list = async (req: Request, res: Response) => {
         res.redirect("/");
     }
 };
+
+// [GET] /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+    const slugSong: string = req.params.slugSong;
+
+    const song = await Song.findOne({
+        slug: slugSong,
+        deleted: false,
+        status: "active"
+    });
+
+    const singer = await Singer.findOne({
+        _id: song.singerId,
+        deleted: false
+    }).select("fullName");
+
+    const topic = await Topic.findOne({
+        _id: song.topicId,
+        deleted: false
+    }).select("title");
+
+    res.render("client/pages/songs/detail", {
+        pageTitle: "Chi tiết bài hát",
+        song: song,
+        singer: singer,
+        topic: topic
+    });
+};
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,39 @@
+// Aplayer
+const aplayer = document.querySelector("#aplayer");
+if(aplayer) {
+    let dataSong = aplayer.getAttribute("data-song");
+    dataSong = JSON.parse(dataSong);
+
+    let dataSinger = aplayer.getAttribute("data-singer");
+    dataSinger = JSON.parse(dataSinger);
+
+    const ap = new APlayer({
+        container: aplayer,
+        audio: [
+            {
+                name: dataSong.title,
+                artist: dataSinger.fullName,
+                url: dataSong.audio,
+                cover: dataSong.avatar
+            },
+        ],
+        autoplay: true,
+        volume: 0.8
+    });
+
+    const avatar = document.querySelector(".singer-detail .inner-avatar");
+
+    ap.on("play", () => {
+        avatar.style.animationPlayState = "running"; 
+    });
+
+    ap.on("pause", () => {
+        avatar.style.animationPlayState = "paused"; 
+    });
+    
+    ap.on("ended", () => {
+        avatar.style.animationPlayState = "paused"; 
+    });
+}
+// End Aplayer
 
