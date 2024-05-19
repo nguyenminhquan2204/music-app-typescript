d72fb3e7f5699e646f8f9dca92eb176830758f71
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-20 01:06:44 +0700

    Làm trang bài hát yêu thích

--- public/js/script.js
+++ public/js/script.js
@@ -68,27 +68,29 @@ if(buttonLike) {
 // End Button Like
 
 // Button Favorite
-const buttonFavorite = document.querySelector("[button-favorite]");
-if(buttonFavorite) {
-    buttonFavorite.addEventListener("click", () => {
-        const idSong = buttonFavorite.getAttribute("button-favorite");
-        const isActive = buttonFavorite.classList.contains("active");
-
-        const typeFavorite = isActive ? "unfavorite" : "favorite";
-
-        const link = `/songs/favorite/${typeFavorite}/${idSong}`;
-
-        const option = {
-            method: "PATCH"
-        }
-
-        fetch(link, option)
-            .then(res => res.json())
-            .then(data => {
-                if(data.code == 200) {
-                    buttonFavorite.classList.toggle("active");
-                }
-            })
+const listButtonFavorite = document.querySelectorAll("[button-favorite]");
+if(listButtonFavorite.length > 0) {
+    listButtonFavorite.forEach((buttonFavorite) => {
+        buttonFavorite.addEventListener("click", () => {
+            const idSong = buttonFavorite.getAttribute("button-favorite");
+            const isActive = buttonFavorite.classList.contains("active");
+    
+            const typeFavorite = isActive ? "unfavorite" : "favorite";
+    
+            const link = `/songs/favorite/${typeFavorite}/${idSong}`;
+    
+            const option = {
+                method: "PATCH"
+            }
+    
+            fetch(link, option)
+                .then(res => res.json())
+                .then(data => {
+                    if(data.code == 200) {
+                        buttonFavorite.classList.toggle("active");
+                    }
+                })
+        });
     });
 }
 // End Button Favorite
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,12 +1,16 @@
 import { Express } from "express";
 import { topicRouters } from "./topic.route";
 import { songRouters } from "./song.route";
+import { favoriteSongRoutes } from "./favorite-song.route";
 
 const clientRoutes = (app: Express): void => {
 
     app.use("/topics", topicRouters);
 
     app.use("/songs", songRouters);
+
+    app.use("/favorite-songs", favoriteSongRoutes);
+
 };
 
 export default clientRoutes;
