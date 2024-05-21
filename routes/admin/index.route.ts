7bc5f5bf8fd8b320f4154bdf06cb96234b0c6b00
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-21 17:12:17 +0700

    Làm trang quản lý chủ đề (Admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
 import { dashboardRoutes } from "./dashboard.route";
 import { systemConfig } from "../../config/config";
+import { topicRoutes } from "../admin/topic.route";
 
 const adminRoutes = (app: Express): void => {
     
@@ -8,6 +9,8 @@ const adminRoutes = (app: Express): void => {
 
     app.use(`${PATH_ADMIN}/dashboard`, dashboardRoutes);
     
+    app.use(`${PATH_ADMIN}/topics`, topicRoutes);
+
 };
 
 export default adminRoutes;
