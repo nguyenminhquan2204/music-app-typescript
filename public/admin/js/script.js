f3c5d95fcb26bcbedc2949188a05424fdfe43697
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-22 00:22:47 +0700

    Tính năng nghe thửc audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,8 +1,8 @@
 // Upload Image
 const uploadImage = document.querySelector("[upload-image]");
 if(uploadImage) {
-    const uploadImageInput = document.querySelector("[upload-image-input]");
-    const uploadImagePreview = document.querySelector("[upload-image-preview]");
+    const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+    const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
 
     uploadImageInput.addEventListener("change", (e) => {
         console.log(e);
@@ -14,3 +14,22 @@ if(uploadImage) {
     });
 }
 // End Upload Image
+
+// Upload Audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if(uploadAudio) {
+    const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+    const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+    const source = uploadAudio.querySelector("source");
+
+    uploadAudioInput.addEventListener("change", (e) => {
+        // console.log(e);
+        const file = e.target.files[0];
+        
+        if(file) {
+            source.src = URL.createObjectURL(file);
+            uploadAudioPlay.load();
+        }
+    });
+}
+// End Upload Audio
