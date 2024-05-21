ae764dd8c884af5724b094af708f8eef3f47d5ea
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-21 16:42:33 +0700

    Dựng layout chung bên Admin

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import path from "path";
 import * as database from "./config/database";
 
 import clientRoutes from "./routes/client/index.route";
+import adminRoutes from "./routes/admin/index.route";
+import { systemConfig } from "./config/config";
+
 
 dotenv.config();
 
@@ -16,8 +20,21 @@ app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+// TinyMCE
+app.use(
+    "/tinymce",
+    express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End TinyMCE
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 //  Client Routers
 clientRoutes(app);
+
+// Admin Routers
+adminRoutes(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
